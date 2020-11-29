e4cf168f0ea82b3e7f5be22c1181a7902c7d26d1
jadeplanet <jade_springreen@naver.com>
2020-11-29 15:29:38 +0900

    chat structure - page UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import ChatPage from './components/ChatPage/ChatPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import firebase from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from './redux/actions/user_action';
 
-function App() {
+function App(props) {
+	let history = useHistory();
+	let dispatch = useDispatch();
+	const isLoading = useSelector((state) => state.user.isLoadng);
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			console.log('user', user);
+			// 로그인이 된 상태
+			if (user) {
+				history.push('/');
+				dispatch(setUser(user));
+			} else {
+				history.push('/login');
+			}
+		});
+	}, []);
+
+	if (isLoading) {
+		return <div>...loading</div>;
+	}
+
 	return (
-		<Router>
-			<Switch>
-				<Route exact path='/' component={ChatPage} />
-				<Route exact path='/login' component={LoginPage} />
-				<Route exact path='/register' component={RegisterPage} />
-			</Switch>
-		</Router>
+		<Switch>
+			<Route exact path='/' component={ChatPage} />
+			<Route exact path='/login' component={LoginPage} />
+			<Route exact path='/register' component={RegisterPage} />
+		</Switch>
 	);
 }
 
---
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DirectMessages = () => {
+	return <div>DirecMessages</div>;
+};
+
+export default DirectMessages;
