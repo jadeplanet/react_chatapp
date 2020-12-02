1f9b28cb780477500778cae78520ed2c0682b26e
jadeplanet <jade_springreen@naver.com>
2020-12-02 18:12:30 +0900

    open room - image upload

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import firebase from '../../../firebase';
 import { useSelector } from 'react-redux';
+import mime from 'mime-types';
 
 const MessageForm = () => {
 	const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
@@ -13,6 +14,8 @@ const MessageForm = () => {
 	const [errors, setErrors] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const messagesRef = firebase.database().ref('messages');
+	const inputOpenImageRef = useRef();
+	const storageRef = firebase.storage().ref();
 
 	const handleChange = (event) => {
 		setContent(event.target.value);
@@ -53,7 +56,23 @@ const MessageForm = () => {
 			setLoading(false);
 			setTimeout(() => {
 				setErrors([]);
-			}, []);
+			}, 5000);
+		}
+	};
+
+	const handleOpenImageRef = () => {
+		inputOpenImageRef.current.click();
+	};
+
+	const handleUploadImage = async (event) => {
+		const file = event.target.files[0];
+		const filePath = `/message/public/${file.name}`;
+		const metadata = { contentType: mime.lookup(file.name) };
+
+		try {
+			await storageRef.child(filePath).put(file, metadata);
+		} catch (error) {
+			alert(error);
 		}
 	};
 	return (
@@ -87,11 +106,21 @@ const MessageForm = () => {
 					</button>
 				</Col>
 				<Col>
-					<button className='message-form-button' style={{ width: '100%' }}>
+					<button
+						onClick={handleOpenImageRef}
+						className='message-form-button'
+						style={{ width: '100%' }}
+					>
 						UPLOAD
 					</button>
 				</Col>
 			</Row>
+			<input
+				style={{ display: 'none' }}
+				type='file'
+				ref={inputOpenImageRef}
+				onChange={handleUploadImage}
+			/>
 		</div>
 	);
 };
